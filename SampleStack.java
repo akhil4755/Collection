cb76de448423b14c57618335feb84c4804a5765b
fahad-kp <fahadkp999@gmail.com>
2021-10-02 00:38:16 +0530

    Sample Stack

---
+++ SampleStack.java
@@ -0,0 +1,18 @@
+import java.util.*;  
+
+public class SampleStack{  
+    public static void main(String args[]){  
+        Stack<String> stack = new Stack<String>();  
+        stack.push("Afin");  
+        stack.push("Kevin");  
+        stack.push("Naveen");  
+        stack.push("Fahad");
+        stack.pop();
+        
+        Iterator<String> itr=stack.iterator();  
+        
+        while(itr.hasNext()){  
+            System.out.println(itr.next());  
+        }  
+    }  
+}  
